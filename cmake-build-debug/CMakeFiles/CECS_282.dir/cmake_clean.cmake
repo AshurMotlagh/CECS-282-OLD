20bb7960c8ec722a47789fe74473ef15d38764be
Ashur Motlagh <48599194+AshurMotlagh@users.noreply.github.com>
2021-01-21 23:58:02 -0800

    First Commit

---
+++ cmake-build-debug/CMakeFiles/CECS_282.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CECS_282.exe"
+  "CECS_282.exe.manifest"
+  "CECS_282.lib"
+  "CECS_282.pdb"
+  "CMakeFiles/CECS_282.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CECS_282.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CECS_282.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CECS_282.
+# This may be replaced when dependencies are built.
